46018dab7e23e74ea36f948b909bf6aae6ad4e6c
ThucTran <thucspc@gmail.com>
2020-10-22 10:32:07 +0700

    22/10/2020_10:32:07,80_1

---
+++ N-update.bat
@@ -0,0 +1,18 @@
+
+@ECHO OFF
+@echo Started: %date% %time%
+
+set /p commit=Enter Commit:
+echo %commit%
+
+
+call git config --global user.email "thucspc@gmail.com"
+call git config --global user.name "ThucTran"
+call git pull origin master
+call git add .
+
+
+call git commit -am %date%_%time%_%commit%
+call npm version patch
+call git push origin master -f
+PAUSE
